d57cdbedfd8d87afb93f3bebfce4f6cfb490b305
brayanramirezart <brayanhramirez@gmail.com>
2022-09-16 11:38:25 -0500

    se agrega el proyecto

---
+++ BRAYAN_ANDRES_C#/ArraysStringIntObJ/ArraysStringIntObJ/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysStringIntObJ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                Console.WriteLine(array[i]);
+
+            }
+            */
+
+
+
+
+
+            /*
+            // Step 1: Array Declaration
+            string[] stringarray;
+
+            // Step 2:Array Initialization
+            stringarray = new string[4] { "Element 1", "Element 2", "Element 3", "Element 4" };
+
+            // Step 3:Accessing Array Elements
+            /*Console.WriteLine(stringarray[0]);
+            Console.WriteLine(stringarray[1]);
+            Console.WriteLine(stringarray[2]);
+            Console.WriteLine(stringarray[3]);
+            */
+            /*
+            for (int i = 0; i < stringarray.Length; i++)
+            {
+
+                Console.WriteLine(stringarray[i]);
+
+            */
+
+                cubiertos[] lacocina = new cubiertos[7];
+                 for (byte i = 0; i < 7; i++)
+                     lacocina[i] = new cubiertos();
+                
+
+            }
+
+
+
+        }
+
+
+        
+        public class cubiertos
+        {
+
+
+            public cubiertos()
+            {
+                Console.WriteLine("objetos para comer");
+            }
+
+        }
+
+        public class cuchara : cubiertos
+
+        {
+
+            public cuchara()
+            {
+                Console.WriteLine("esta es la cuchara");
+            }
+
+
+        }
+
+        public class tenedor : cubiertos
+
+        {
+
+            public tenedor()
+            {
+                Console.WriteLine("este es el tenedor");
+            }
+
+
+        }
+        
+    }
+
+
+
---
+++ BRAYAN_ANDRES_C#/ProyectoHerencia/ProyectoHerencia/Program.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoHerencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Caballo Babieca = new Caballo("blackhorse");
+            Humano Brayan = new Humano("Brayan");
+            Gorila Copito = new Gorila("kong");
+
+            //Brayan.pensar(1);
+
+            /* Brayan.getNombre();
+             Brayan.nombreServivo = "lo q quiera";
+             Brayan.getNombre();
+
+
+             /*
+             Babieca.getNombre();
+             Brayan.getNombre();
+
+
+
+             Babieca.Galopar();
+             Babieca.getNombre();
+             */
+
+            /*  principio de sustitucion aplicado 
+
+             Mamiferos animal = new Mamiferos("estrella");
+             Caballo estrella = new Caballo("estrella");
+             animal = estrella;
+
+           */
+
+            Mamiferos[] AlmacenDeAnimales = new Mamiferos[3];
+
+            AlmacenDeAnimales[0] = Babieca;
+            AlmacenDeAnimales[1] = Brayan;
+            AlmacenDeAnimales[2] = Copito;
+
+            for (int i = 0; i < 3; i++)
+
+            {
+                AlmacenDeAnimales[i].pensar();
+
+            }
+
+            // Mamiferos miMamifero = new Mamiferos("");
+            //miMamifero.respirar();
+           
+            
+        }
+    }
+
+
+    class Mamiferos
+    {
+
+   // Mamiferos miMamifero = new Mamiferos("");
+
+        //Humano Brayan = new Humano("Brayan");
+       
+
+        public Mamiferos(String nombre)
+
+        {
+            this.nombreServivo = nombre;         
+
+        }
+
+        public virtual void pensar()
+        {
+
+            Console.WriteLine("pensamiento basico instintivo");
+
+        }
+
+        /*
+        public Caballo(string nombreCaballo) : base(nombreCaballo)
+
+        {
+
+        }
+        */
+
+        public void respirar()
+        {
+            Console.WriteLine("Soy capaz de respirar");
+        }
+
+        public void CuidarCrias()
+        {
+            Console.WriteLine("Cuido de mis crias hasta que se valgan por si solas");
+        }
+
+        public void getNombre()
+        {
+            Console.WriteLine("el nombre del ser vivo es: " + nombreServivo);
+        }
+
+        private String nombreServivo;
+
+
+    }
+
+    class Caballo : Mamiferos
+    {
+
+        public Caballo(string nombreCaballo) : base(nombreCaballo)
+
+        {
+
+        }
+        
+
+        public void Galopar()
+        {
+            Console.WriteLine("soy capaz de galopar");
+        }
+        
+    }
+
+    class Humano : Mamiferos
+    {
+        public Humano(string nombreHumano) : base(nombreHumano)
+
+        {
+
+        }
+
+        public override void pensar()
+        {
+            Console.WriteLine("Soy capaz de pensar");
+
+
+        }
+
+        public void pensar(string pensar)
+        {
+            Console.WriteLine("Soy capaz de pensar");
+
+
+        }
+
+        public void pensar(int pensar)
+        {
+            Console.WriteLine("pensamienro numero");
+
+
+        }
+
+
+        /*
+                public new void pensar()
+            {
+
+            }
+
+            */
+
+    }
+
+    class Gorila : Mamiferos {
+
+
+
+        public Gorila(string nombreGorila) : base(nombreGorila)
+
+        {
+            
+
+        }
+
+
+        public void trepar()
+        {
+            Console.WriteLine("Soy capaz de trepar");
+        }
+
+        public override void pensar()
+        {
+            Console.WriteLine("pensamiento instintivo avanzado");
+
+
+        }
+
+    }
+}
+
